a28ea08a21e45ed664ee737ab40676f71077557a
jesus_lopez <jesus.lopez@sonatas.com.mx>
2015-07-03 15:56:44 -0700

    update alot of information about people and their areas, animations, navbar functionality, changes made by the client

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,7 +4,21 @@
  	$( '#cbp-qtrotator2' ).cbpQTRotator();
  	$('.button-collapse').sideNav({menuWidth: 240, activationWidth: 70});
  	$('#jsCarousel').jsCarousel({ autoscroll: true });
-
  	$('.modal-trigger').leanModal();
  	new WOW().init();
- });
+ 	            // Prevent console.log from generating errors in IE for the purposes of the demo
+ 	            if ( ! window.console ) console = { log: function(){} };
+
+            // The actual plugin
+            $('.single-page-nav').singlePageNav({
+            	offset: $('.single-page-nav').outerHeight(),
+            	filter: ':not(.external)',
+            	updateHash: true,
+            	beforeStart: function() {
+            		console.log('begin scrolling');
+            	},
+            	onComplete: function() {
+            		console.log('done scrolling');
+            	}
+            });
+        });
